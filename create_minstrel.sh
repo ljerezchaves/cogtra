e37903e2fd2e4fc8fe0d99d8e00257502de66cb4
Bruno Guimaraes <brunoguic@gmail.com>
2013-02-17 20:27:04 +0000

    Adding create.sh
    git-svn-id: https://cogtra.googlecode.com/svn/trunk@73 6fde1a75-d58d-6ec5-3f83-e29649f9c9df

---
+++ create_minstrel.sh
@@ -0,0 +1,27 @@
+cp package/mac80211/Makefile.old package/mac80211/Makefile
+rm package/mac80211/patches/920*
+rm package/mac80211/patches/930*
+
+make package/mac80211/clean V=99
+make package/mac80211/prepare V=99
+
+cp /home/brunoguic/cogtra/rc80211_minstrel* /home/brunoguic/openwrt/build_dir/linux-ar71xx/compat-wireless-2011*/net/mac80211/
+
+make package/mac80211/compile V=99
+
+rm /home/brunoguic/Downloads/minstrel/kmod*
+
+cp /home/brunoguic/openwrt/bin/ar71xx/packages/kmod-ath* /home/brunoguic/Downloads/minstrel/
+cp /home/brunoguic/openwrt/bin/ar71xx/packages/kmod-cfg* /home/brunoguic/Downloads/minstrel/
+cp /home/brunoguic/openwrt/bin/ar71xx/packages/kmod-mac* /home/brunoguic/Downloads/minstrel/
+
+
+ssh rspro 'rm kmod*;exit'
+
+scp /home/brunoguic/openwrt/bin/ar71xx/packages/kmod-ath* rspro:~/minstrel/
+scp /home/brunoguic/openwrt/bin/ar71xx/packages/kmod-cfg* rspro:~/minstrel/
+scp /home/brunoguic/openwrt/bin/ar71xx/packages/kmod-mac* rspro:~/minstrel/
+
+ssh rspro 'cd minstrel;./install;exit'
+
+ssh rspro
