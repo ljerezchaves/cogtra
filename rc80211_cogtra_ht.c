cd8e61c0704df58f6969764d8f81fd39dd814915
Bruno Guimaraes <brunoguic@gmail.com>
2013-03-18 14:15:43 +0000

    git-svn-id: https://cogtra.googlecode.com/svn/trunk@119 6fde1a75-d58d-6ec5-3f83-e29649f9c9df

--- rc80211_cogtra_ht.c
+++ rc80211_cogtra_ht.c
@@ -419,7 +419,7 @@ static void cogtra_ht_update_stats (struct cogtra_priv *cp, struct cogtra_ht_sta
 		if (( minstrel_mcs_groups[random_rate_gix].duration[random_rt] > minstrel_mcs_groups[max_tp_rate_gix].duration[max_tp_rate]) || 
 (ci->groups[random_rate_gix].rates[random_rt].avg_prob < 180)){
 			ci->update_interval = COGTRA_HT_RECOVERY_INTERVAL;
-			printk("r:%u | t:%u\n", ci->random_rate_mcs,ci->random_rate_mcs );
+			printk("r:%u | t:%u\n | p:%u", ci->random_rate_mcs,ci->random_rate_mcs,ci->groups[random_rate_gix].rates[random_rt].avg_prob );
 		}else{
 			ci->update_interval = COGTRA_HT_UPDATE_INTERVAL;
 		}
